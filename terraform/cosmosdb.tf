09b74950edc9f15e759ab9496c83556eaef639e8
Bruno Ramalho <bfmramalho@gmail.com>
2020-02-23 12:21:04 +0100

    cosmos db

--- terraform/cosmosdb.tf
+++ terraform/cosmosdb.tf
@@ -1,22 +1,43 @@
-data "azurerm_cosmosdb_account" "db_account" {
+resource "azurerm_cosmosdb_account" "db_account" {
+    name                = "${var.prefix}-db-account"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    offer_type          = "Standard"
+    kind                = "GlobalDocumentDB"
+
+    enable_automatic_failover = false
+
+    consistency_policy {
+        consistency_level       = "BoundedStaleness"
+        max_interval_in_seconds = 10
+        max_staleness_prefix    = 200
+    }
+
+    geo_location {
+        prefix            = "${var.prefix}-db-customid"
+        location          = azurerm_resource_group.rg.location
+        failover_priority = 0
+    }
+}
+
+resource "azurerm_cosmosdb_sql_database" "db" {
     name                = "${var.prefix}-db-account"
     resource_group_name = azurerm_resource_group.rg.name
-}
+    account_name        = azurerm_cosmosdb_account.db_account.name
 
-resource "azurerm_cosmosdb_mongo_database" "db" {
-    name                = "${var.prefix}-db"
-    resource_group_name = azurerm_resource_group.rg.name
-    account_name        = data.azurerm_cosmosdb_account.db_account.name
     throughput          = 400
 }
 
-resource "azurerm_cosmosdb_mongo_collection" "db_collection" {
-    name                = "${var.prefix}-db-collection"
+resource "azurerm_cosmosdb_sql_container" "container" {
+    name                = "${var.prefix}-container"
     resource_group_name = azurerm_resource_group.rg.name
-    account_name        = data.azurerm_cosmosdb_account.db_account.name
-    database_name       = azurerm_cosmosdb_mongo_database.db.name
-    
-    default_ttl_seconds = "777"
-    shard_key           = "uniqueKey"
+    account_name        = azurerm_cosmosdb_account.db_account.name
+    database_name       = azurerm_cosmosdb_sql_database.db.name
+
+    partition_key_path  = "/definition/id"
     throughput          = 400
+
+    unique_key {
+        paths = ["/definition/idlong", "/definition/idshort"]
+    }
 }
